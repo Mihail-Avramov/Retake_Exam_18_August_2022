1d35625d000c51bf351e2699482e98da8e2a9a6c
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-06 12:22:49 +0200

    Add project files.

---
+++ Barista Contest 3 1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Barista_Contest_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> coffeeQuantities = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> milkQuantities = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            Dictionary<string, int> coffeeDrinksMenue = new Dictionary<string, int>()
+            {
+                {"Cortado", 50},
+                {"Espresso", 75},
+                {"Capuccino", 100},
+                {"Americano", 150},
+                {"Latte", 200}
+            };
+
+            Dictionary<string, int> coffeeMade = new Dictionary<string, int>();
+
+            while (coffeeQuantities.Any() && milkQuantities.Any())
+            {
+                int totalQuantities = coffeeQuantities.Peek() + milkQuantities.Peek();
+                if (coffeeDrinksMenue.ContainsValue(totalQuantities))
+                {
+                    string drinkName = coffeeDrinksMenue.First(c => c.Value == totalQuantities).Key;
+
+                    if (!coffeeMade.ContainsKey(drinkName))
+                    {
+                        coffeeMade.Add(drinkName, 0);
+                    }
+
+                    coffeeMade[drinkName]++;
+
+                    coffeeQuantities.Dequeue();
+                    milkQuantities.Pop();
+                }
+                else
+                {
+                    coffeeQuantities.Dequeue();
+                    milkQuantities.Push(milkQuantities.Pop() - 5);
+                }
+            }
+
+            if (coffeeQuantities.Count == 0 && milkQuantities.Count == 0)
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            }
+            else
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            }
+
+            if (coffeeQuantities.Count > 0)
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffeeQuantities)}");
+            }
+            else
+            {
+                Console.WriteLine("Coffee left: none");
+            }
+
+            if (milkQuantities.Count > 0)
+            {
+                Console.WriteLine($"Milk left: {string.Join(", ", milkQuantities)}");
+            }
+            else
+            {
+                Console.WriteLine("Milk left: none");
+            }
+
+            foreach (var drink in coffeeMade.OrderBy(c => c.Value).ThenByDescending(c => c.Key))
+            {
+                Console.WriteLine($"{drink.Key}: {drink.Value}");
+            }
+        }
+    }
+}
---
+++ Help-A-Mole/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace Help_A_Mole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            char[,] field = new char[fieldSize, fieldSize];
+
+            int mRow = -1;
+            int mCol = -1;
+            int s1Row = -1;
+            int s1Col = -1;
+            int s2Row = -1;
+            int s2Col = -1;
+
+            int totalPoints = 0;
+
+            for (int row = 0; row < fieldSize; row++)
+            {
+                string colValues = Console.ReadLine();
+                for (int col = 0; col < fieldSize; col++)
+                {
+                    field[row,col] = colValues[col];
+
+                    if (colValues[col] == 'M')
+                    {
+                        mRow = row;
+                        mCol = col;
+                        continue;
+                    }
+
+                    if (colValues[col] == 'S')
+                    {
+                        if (s1Row == -1)
+                        {
+                            s1Row = row;
+                            s1Col = col;
+                        }
+                        else
+                        {
+                            s2Row = row;
+                            s2Col = col;
+                        }
+                        continue;
+                    }
+                }
+            }
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End" && totalPoints < 25)
+            {
+                switch (input)
+                {
+                    case "up":
+                    {
+                        int indexToCheck = mRow - 1;
+                        if (isValidIndex(indexToCheck, fieldSize))
+                        {
+                            field[mRow, mCol] = '-';
+                            mRow = indexToCheck;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Don't try to escape the playing field!");
+                        }
+
+                        break;
+                    }
+                    case "down":
+                    {
+                        int indexToCheck = mRow + 1;
+                        if (isValidIndex(indexToCheck, fieldSize))
+                        {
+                            field[mRow, mCol] = '-';
+                            mRow = indexToCheck;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Don't try to escape the playing field!");
+                        }
+
+                        break;
+                    }
+                    case "left":
+                    {
+                        int indexToCheck = mCol - 1;
+                        if (isValidIndex(indexToCheck, fieldSize))
+                        {
+                            field[mRow, mCol] = '-';
+                            mCol = indexToCheck;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Don't try to escape the playing field!");
+                        }
+
+                        break;
+                    }
+                    case "right":
+                    {
+                        int indexToCheck = mCol + 1;
+                        if (isValidIndex(indexToCheck, fieldSize))
+                        {
+                            field[mRow, mCol] = '-';
+                            mCol = indexToCheck;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Don't try to escape the playing field!");
+                        }
+
+                        break;
+                    }
+                }
+
+                if (field[mRow, mCol] == 'S')
+                {
+                    totalPoints -= 3;
+                    field[mRow, mCol] = '-';
+                    if (mRow == s1Row && mCol == s1Col)
+                    {
+                        mRow = s2Row;
+                        mCol = s2Col;
+                    }
+                    else
+                    {
+                        mRow = s1Row;
+                        mCol = s1Col;
+                    }
+                    field[mRow, mCol] = 'M';
+                    continue;
+                }
+
+                if (char.IsDigit(field[mRow, mCol]))
+                {
+                    int pointsToAdd = (int)field[mRow, mCol] - 48;
+                    totalPoints += pointsToAdd;
+                    field[mRow, mCol] = 'M';
+                    continue;
+                }
+
+                field[mRow, mCol] = 'M';
+
+            }
+
+            if (totalPoints >= 25)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {totalPoints} points.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {totalPoints} points.");
+            }
+
+            for (int row = 0; row < fieldSize; row++)
+            {
+                for (int col = 0; col < fieldSize; col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool isValidIndex(int indexToCheck, int fieldSize)
+        {
+            return indexToCheck >= 0 && indexToCheck < fieldSize;
+        }
+    }
+}
